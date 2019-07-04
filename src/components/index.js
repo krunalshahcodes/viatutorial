4ff8701654398b99e11205315ca5627d4191fbe9
Krunal Shah <shahkrunal7091@gmail.com>
2019-07-04 12:45:54 +0530

    clean repo

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Layout from './Layout'
-import Container from './Container'
-import SEO from './SEO'
-import PostCard from './PostCard'
-import PostDetails from './PostDetails'
-
-export { Layout, Container, SEO, PostCard, PostDetails }
