55bbe75e4aab55861aa6d6e2c9bbca052b223639
Krunal Shah <shahkrunal7091@gmail.com>
2019-08-11 16:05:03 +0530

    chore: update lint staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  linters: {
-    '*.{js,jsx}': ['yarn run lint:fix', 'eslint'],
-    '*.+(json|yml|yaml|css|less|scss|md|graphql|mdx)': ['prettier --write', 'git add'],
-  },
+  '*.{js,jsx}': ['yarn run lint:fix', 'eslint'],
+  '*.+(json|yml|yaml|css|less|scss|md|graphql|mdx)': ['prettier --write', 'git add'],
 }
