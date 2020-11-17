553e09aabe50e4235cdb98bf7220ff735180fe86
Krunal Shah <shahkrunal7091@gmail.com>
2020-11-17 21:38:11 +0530

    chore: setup plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,54 @@
+const path = require('path')
+const config = require('./src/common/config')
+
 module.exports = {
-  plugins: [],
+  siteMetadata: {
+    siteTitle: config.siteTitle,
+    siteDescription: config.siteDescription,
+    siteName: config.siteName,
+    siteUrl: config.siteUrl,
+    twitter: config.twitter,
+    facebook: config.facebook,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: path.join(process.cwd(), 'content'),
+      },
+    },
+    `gatsby-plugin-mdx`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `@chakra-ui/gatsby-plugin`,
+    `gatsby-plugin-nprogress`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: config.googleAnalyticsID,
+        anonymize: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: config.siteUrl,
+        sitemap: `${config.siteUrl}/sitemap.xml`,
+        env: {
+          development: {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+          },
+          production: {
+            policy: [{ userAgent: '*', disallow: '/' }],
+          },
+        },
+      },
+    },
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-netlify-cache`,
+  ],
 }
---
+++ src/common/config.js
@@ -0,0 +1,20 @@
+const meta = {
+  siteTitle: 'Learn, Code and Share Via Tutorial',
+  siteName: 'Via Tutorial',
+  siteDescription:
+    'Via Tutorial is a platform that gives you access to thousands of coding resources allowing you to learn at your own pace.',
+  siteUrl: 'https://www.viatutorial.com',
+}
+
+const social = {
+  twitter: '@viatutorial',
+  facebook: 'viatutorial',
+}
+
+const config = {
+  ...meta,
+  ...social,
+  googleAnalyticsID: 'UA-104250012-2',
+}
+
+module.exports = config
